0a3f9a6add3a7d04794edf4e0af1d250f5674bbe
nkarki13731 <nkarki13731@upei.ca>
2024-05-10 20:00:01 -0600

    scalar method added and implementing raytracer class

--- Raytracer.java
+++ Raytracer.java
@@ -1,2 +1,11 @@
 public class Raytracer {
+    private  int w;
+    private  int h;
+    private  Scene scene;
+
+    public Raytracer(int w, int h,Scene scene){
+        this.w = w;
+        this.h = h;
+        this.scene = scene;
+    }
 }
